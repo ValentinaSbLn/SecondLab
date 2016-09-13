15c24dbefc6e1f40d2e591321054b1cded55d20e
ValentinaSbLn <Aliara@yandex.ru>
2016-09-13 11:14:53 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Valentina on 13.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Person Harry = new Person(true, "Harry");
+        Person Mary = new Person(false, "Mary");
+        Person Anna = new Person(false, "Anna");
+        Person David = new Person(true, "David");
+
+       if (Harry.marry(Mary))
+           System.out.println("Harry и Mary поженились");
+       else
+           System.out.println("Не поженились");
+
+        if (David.marry(Anna))
+            System.out.println("David и Anna поженились");
+        else
+            System.out.println("Не поженились");
+
+        if (David.marry(Mary))
+            System.out.println("David и Mary поженились");
+        else
+            System.out.println("Не поженились");
+
+        if (Harry.marry(Anna))
+            System.out.println("Harry и Anna поженились");
+        else
+            System.out.println("Не поженились");
+
+        if (Mary.marry(Anna))
+            System.out.println("Mary и Anna поженились");
+        else
+            System.out.println("Не поженились");
+
+        if (David.marry(Harry))
+            System.out.println("Mary и Anna поженились");
+        else
+            System.out.println("Не поженились");
+
+
+
+    }
+}
